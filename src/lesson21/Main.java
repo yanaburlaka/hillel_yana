4d0fbbcce3a505c7bfc75843a24dab6802fbd71a
Yana Burlaka <yburlaka@slice.com>
2019-02-26 19:52:10 +0200

    partly 21 task

--- src/lesson21/Main.java
+++ src/lesson21/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyMap<String, Integer> myMap = new MyMap<String, Integer>();
+        MyMap<String, Integer> countriesAndArea = new MyMap<String, Integer>();
+        countriesAndArea.put("Canada", 9_984_670);
+        countriesAndArea.put("United States of America", 9_826675);
+        countriesAndArea.put("China", 9_598_094);
+        countriesAndArea.put("Argentina", 2_766_890);
+        System.out.println("Sdf");
     }
 }
--- src/lesson21/MyMap.java
+++ src/lesson21/MyMap.java
@@ -1,8 +1,16 @@
 package lesson21;
 
+import java.util.Arrays;
+
 public class MyMap<K, V> {
 
     private Entry<K, V>[] entries;
+    private static final int SIZE = 4;
+    private int sizeOfBucket = 0;
+
+    public MyMap() {
+        this.entries = new Entry[SIZE];
+    }
 
     private class Entry<K, V> {
         K key;
@@ -17,15 +25,48 @@ public class MyMap<K, V> {
     }
 
     public void put(K key, V value) {
-        int index = getHash(key);
-        storeEntry(key, value, index);
+
+        Entry<K, V> newEntry = new Entry(key, value, null);
+        int index = getHashCode(key);
+        Entry<K, V> existingEntry = entries[index];
+        if (existingEntry == null) {
+            entries[index] = newEntry;
+            sizeOfBucket++;
+        } else {
+            while (existingEntry.next != null) {
+                if (existingEntry.key.equals(key)) {
+                    existingEntry.value = value;
+                    return;
+                }
+                existingEntry = existingEntry.next;
+            }
+            if (existingEntry.key.equals(key)) {
+                existingEntry.value = value;
+            } else {
+                existingEntry.next = newEntry;
+                sizeOfBucket++;
+            }
+        }
     }
 
-    private void storeEntry(K key, V value, int index) {
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyMap<?, ?> myMap = (MyMap<?, ?>) o;
+
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(entries, myMap.entries);
     }
 
-    private int getHash(K key) {
-        int hashCode = key.hashCode();
-        return hashCode % 100;
+
+    public int getHashCode(K key) {
+        String s = key.toString();
+        int i = s.length() % SIZE;
+        return i;
     }
+
 }

831f8cccb487e4cae2c3952c72e9c1d9b1047fc1
Yana Burlaka <yburlaka@slice.com>
2019-03-06 10:01:41 +0200

    partly 21 task

--- src/lesson21/MyMap.java
+++ src/lesson21/MyMap.java
@@ -1,8 +1,11 @@
 package lesson21;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
-public class MyMap<K, V> {
+public class MyMap<K, V> implements Map{
 
     private Entry<K, V>[] entries;
     private static final int SIZE = 4;
@@ -13,8 +16,8 @@ public class MyMap<K, V> {
     }
 
     private class Entry<K, V> {
-        K key;
-        V value;
+        Object key;
+        Object value;
         Entry<K, V> next;
 
         public Entry(K key, V value, Entry<K, V> next) {
@@ -29,35 +32,74 @@ public class MyMap<K, V> {
                     '}';
         }
     }
+//
+//    public void put(K key, V value) {
+//
+//        Entry<K, V> newEntry = new Entry(key, value, null);
+//        int index = getHashCode(key);
+//        Entry<K, V> existingEntry = entries[index];
+//        if (existingEntry == null) {
+//            entries[index] = newEntry;
+//            sizeOfBucket++;
+//        } else {
+//            while (existingEntry.next != null) {
+//                if (existingEntry.key.equals(key)) {
+//                    existingEntry.value = value;
+//                    return;
+//                }
+//                existingEntry = existingEntry.next;
+//            }
+//            if (existingEntry.key.equals(key)) {
+//                existingEntry.value = value;
+//            } else {
+//                existingEntry.next = newEntry;
+//                sizeOfBucket++;
+//            }
+//        }
+//    }
 
-    public void put(K key, V value) {
+//    public V get(K keyToGet) {
+//        int index = getHashCode(keyToGet);
+//        V valueToGet = null;
+//        Entry<K, V> existingEntry = entries[index];
+//        if (existingEntry == null) {
+//            System.out.println("We don't have this country, sorry");
+//        } else {
+//            while (existingEntry.next != null) {
+//                if (existingEntry.key.equals(keyToGet)) {
+//                    valueToGet = existingEntry.value;
+//                    return valueToGet;
+//                }
+//                existingEntry = existingEntry.next;
+//            }
+//        }
+//        return valueToGet;
+//    }
 
-        Entry<K, V> newEntry = new Entry(key, value, null);
-        int index = getHashCode(key);
-        Entry<K, V> existingEntry = entries[index];
-        if (existingEntry == null) {
-            entries[index] = newEntry;
-            sizeOfBucket++;
-        } else {
-            while (existingEntry.next != null) {
-                if (existingEntry.key.equals(key)) {
-                    existingEntry.value = value;
-                    return;
-                }
-                existingEntry = existingEntry.next;
-            }
-            if (existingEntry.key.equals(key)) {
-                existingEntry.value = value;
-            } else {
-                existingEntry.next = newEntry;
-                sizeOfBucket++;
-            }
-        }
+    @Override
+    public int size() {
+        return sizeOfBucket;
     }
 
-    public V get(K keyToGet) {
+    @Override
+    public boolean isEmpty() {
+        return (sizeOfBucket == 0);
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object keyToGet) {
         int index = getHashCode(keyToGet);
-        V valueToGet = null;
+        Object valueToGet = null;
         Entry<K, V> existingEntry = entries[index];
         if (existingEntry == null) {
             System.out.println("We don't have this country, sorry");
@@ -74,6 +116,61 @@ public class MyMap<K, V> {
     }
 
     @Override
+    public Object put(Object key, Object value) {
+        Entry<K, V> newEntry = new Entry(key, value, null);
+        int index = getHashCode(key);
+        Entry<K, V> existingEntry = entries[index];
+        if (existingEntry == null) {
+            entries[index] = newEntry;
+            sizeOfBucket++;
+        } else {
+            while (existingEntry.next != null) {
+                if (existingEntry.key.equals(key)) {
+                    existingEntry.value = value;
+                }
+                existingEntry = existingEntry.next;
+            }
+            if (existingEntry.key.equals(key)) {
+                existingEntry.value = value;
+            } else {
+                existingEntry.next = newEntry;
+                sizeOfBucket++;
+            }
+        }
+        return value;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Map.Entry> entrySet() {
+        return null;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -85,7 +182,7 @@ public class MyMap<K, V> {
     }
 
 
-    public int getHashCode(K key) {
+    public int getHashCode(Object key) {
         String s = key.toString();
         int i = s.length() % SIZE;
         return i;
